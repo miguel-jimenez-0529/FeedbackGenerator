c57c069f44c112a59ab54ba4e93d8852c1e225fc
Miguel Angel Jimenez <macbookpro@miguelon.local>
2018-04-25 12:46:00 -0500

    Initial Commit

---
+++ FeedbackGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FeedbackGenerator
+//
+//  Created by Macbook Pro on 25/04/18.
+//  Copyright © 2018 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
